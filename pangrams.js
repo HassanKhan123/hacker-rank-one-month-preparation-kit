015af9129514b563552c25947f95984b6acf6f8c
HassanKhan123 <hk2602809@gmail.com>
2023-02-18 16:56:53 +0500

    solved: pangram problem

---
+++ pangrams.js
@@ -0,0 +1,36 @@
+const pangrams = str => {
+  let alphabets = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const res = alphabets.every(al => str.toLowerCase().includes(al));
+
+  return res ? "pangram" : "not pangram";
+};
+
+pangrams("The quick brown fox jumps over the lazy dog");
